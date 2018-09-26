3e6f6e12a20854b5a94b33da5dff509258e68e32
Galih Muhammad <galih0muhammad@gmail.com>
2018-09-26 08:18:31 +0700

    Enable using _config.yml for airtable config

--- lib/jekyll/airtable_fetcher.rb
+++ lib/jekyll/airtable_fetcher.rb
@@ -8,20 +8,14 @@ module Jekyll
     priority :lowest      
 
     def generate(site)
-      return false if site.config['AIRTABLE_API_KEY'].nil? || site.config['AIRTABLE_API_KEY'] == ''
-      return false if site.config['SYNC_WITH_AIRTABLE'] == 'false'
-
-      setup_directories
+      return false if should_generate_be_prevented?
       # For storing hashes of attachments that will be saved to the data file
       @attachments_hash = {}
+      setup_directories
 
-      Airtable.configure do |config|
-        config.api_key = site.config['AIRTABLE_API_KEY']
-      end
+      client = setting_up_airtable_client
 
-      client = Airtable.client(base_uid: site.config['AIRTABLE_BASE_UID'])      
-
-      site.config['AIRTABLE_TABLE_NAMES'].each do |table_name|
+      @table_names.each do |table_name|
         records = client.list_records(table_name: table_name)
         next if records.size == 0
 
@@ -30,18 +24,8 @@ module Jekyll
 
         records.each do |record|
           fields    = record['fields']
-          # We use the first field as the primary key
-          # Then find the value of the primary key to be stored as the slug, which
-          # will be used as file name and the path to the record in the url.
-          # However, if the record has field called 'slug', it will be used instead
-          pkey      = fields.keys.first
-          slug      = fields['slug'].nil? ? fields[pkey] : fields['slug']
-          filename  = to_snake(slug) + '.md'
           uid       = record['id']
-
-          out_file  = File.new("#{directory_name}/#{filename}", "w")
-          out_file.puts(front_matter_mark)
-          out_file.puts("uid: #{uid}")
+          out_file  = create_page_for_the_record(directory_name, uid, fields)
 
           fields.each do |key, value|
             snake_key = to_snake(key)
@@ -66,6 +50,24 @@ module Jekyll
     end
 
     private
+
+    def should_generate_be_prevented?
+      is_enabled    = site.airtable.enable_sync == 'true' || site.config['SYNC_WITH_AIRTABLE'] == 'true'
+      return true if !is_enabled
+      
+      @api_key      = site.config['AIRTABLE_API_KEY']
+      @base_uid     = site.airtable.base_uid || site.config['AIRTABLE_BASE_UID']
+      @table_names  = site.airtable.tables || site.config['AIRTABLE_TABLE_NAMES']
+
+      return true if @api_key.nil? || @api_key == '' || @base_uid.nil? || @base_uid == ''
+      false
+    end
+
+    def setting_up_airtable_client
+      Airtable.configure { |config| config.api_key = @api_key }
+
+      Airtable.client(base_uid: @base_uid)
+    end
 
     def front_matter_mark
       '---'
@@ -113,13 +115,29 @@ module Jekyll
       Dir.mkdir('_data') unless File.exists?('_data')
       Dir.mkdir('_data/airtable') unless File.exists?('_data/airtable')      
 
-      Dir.mkdir('collections') unless File.exists?('collections')      
+      Dir.mkdir('collections') unless File.exists?('collections')
     end
 
     def write_attachments_data_file
       File.open("_data/airtable/attachments.yml", "w") do |f| 
         f.write(@attachments_hash.to_yaml)
       end
+    end
+
+    def create_page_for_the_record(directory_name, uid, fields)
+      # We use the first field as the primary key
+      # Then find the value of the primary key to be stored as the slug, which
+      # will be used as file name and the path to the record in the url.
+      # However, if the record has field called 'slug', it will be used instead
+      pkey      = fields.keys.first
+      slug      = fields['slug'].nil? ? fields[pkey] : fields['slug']
+      filename  = to_snake(slug) + '.md'
+
+      out_file  = File.new("#{directory_name}/#{filename}", "w")
+      out_file.puts(front_matter_mark)
+      out_file.puts("uid: #{uid}")
+
+      out_file
     end
   end
 end
